31d3aa25ad6fb2d1edca5af36edf5dd65985d843
TimmyTheTaterTot <62367370+TimmyTheTaterTot@users.noreply.github.com>
2023-11-08 00:34:39 -0700

    Added Open File support and Proper Image Scaling

--- ImageEditor.py
+++ ImageEditor.py
@@ -11,17 +11,15 @@ class Application:
     def init_window(self):
         # fmt: off
         self.window = tk.Tk()
-        self.window.minsize(600, 300)
-
-        
+        self.window.resizable(False, False)
 
         self.info_frame = tk.Frame(master=self.window, height=20)
         top_spacer_frame = tk.Frame(master=self.window, height=1, background="black")
         self.images_frame = tk.Frame(master=self.window)
 
-        self.in_image_lbl = tk.Label(master=self.images_frame, width=40, height=10, background="grey", text="placeholder") # tk.Label(master=self.images_frame, image=self.input_img)
+        self.in_image_lbl = tk.Label(master=self.images_frame, width=40, height=17, background="grey", text="placeholder") # tk.Label(master=self.images_frame, image=self.input_img)
         middle_spacer_frame = tk.Frame(master=self.images_frame, width=1, background="black")
-        self.out_img_lbl = tk.Label(master=self.images_frame, width=40, height=10, background="grey", text="placeholder")
+        self.out_img_lbl = tk.Label(master=self.images_frame, width=40, height=17, background="grey", text="placeholder")
         load_img_button = tk.Button(master=self.info_frame, text="Load New Image", command=self.handle_load_image)
         self.info_label = tk.Label(master=self.info_frame, text='')
 
@@ -29,16 +27,16 @@ class Application:
         top_spacer_frame.grid(row=1, sticky="nsew")
         self.images_frame.grid(row=2, sticky="nsew")
 
-        self.in_image_lbl.grid(row=0, column=0, sticky="nsew")
-        middle_spacer_frame.grid(row=0, column=1, sticky="ns", padx=5)
-        self.out_img_lbl.grid(row=0, column=2, sticky="nsew")
+        self.in_image_lbl.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        middle_spacer_frame.grid(row=0, column=1, sticky="ns")
+        self.out_img_lbl.grid(row=0, column=2, sticky="nsew", padx=5, pady=5)
         load_img_button.pack(side=tk.LEFT, padx=4, pady=4)
         self.info_label.pack(side=tk.LEFT, padx=4, pady=4)
 
         self.window.rowconfigure(2, weight=1, minsize=200)
         self.window.columnconfigure(0, weight=1, minsize=600)
-        self.images_frame.rowconfigure([0, 2], weight=1)
-        self.images_frame.columnconfigure([0, 2], weight=1)
+        self.images_frame.rowconfigure([0, 2], weight=2)
+        self.images_frame.columnconfigure([0, 2], weight=2)
 
         self.window.mainloop()
         # fmt: on
@@ -58,11 +56,24 @@ class Application:
             self.info_label.config(text="Invalid file path. Please try again.")
             return
         try:
-            self.inpt_img = ImageTk.PhotoImage(
-                Image.open("test_files/beach1.input.jpg")
-            )
+            img = Image.open(filepath)
+            img_width, img_height = img.size
+
+            scalarx = 500/img_width
+            scalary = 400/img_height
+            if scalarx > scalary:
+                img_width *= scalary
+                img_height *= scalary
+            else:
+                img_width *= scalarx
+                img_height *= scalarx
+
+            img.thumbnail((img_width, img_height), Image.Resampling.LANCZOS)
+            self.input_img = ImageTk.PhotoImage(img, size=(img_width, img_height))
         except:
             self.info_label.config(text="Invalid image type or contents")
+
+        self.in_image_lbl.config(image=self.input_img, background="light grey", text="", width=img_width, height=img_height)
 
 
 if __name__ == "__main__":
